5c95c4367c458fc8adb7d008ca9d25077ad14ea5
Wortmann, Jan-Niklas <Jan-Niklas.Wortmann@opitz-consulting.com>
2017-10-17 22:10:55 +0200

    initialize project structure

---
+++ lending/src/main/java/de/jwo/lending/LendingApplication.java
@@ -0,0 +1,12 @@
+package de.jwo.lending;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LendingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LendingApplication.class, args);
+	}
+}
